9b98b57e0beb124a39512a3acb2bfe78600ad818
nidhack <37173849+nidhack@users.noreply.github.com>
2019-01-31 09:11:18 +0530

    Mvs application with form validaion and  database connectivitty

---
+++ New/New/Controllers/UsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using New.Models;
+
+namespace New.Controllers
+{
+    public class UsController : Controller
+    {
+        private UserContext db = new UserContext();
+
+        //
+        // GET: /Us/
+
+        public ActionResult Index()
+        {
+            return View(db.users.ToList());
+        }
+
+        //
+        // GET: /Us/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            New1 new1 = db.users.Find(id);
+            if (new1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(new1);
+        }
+
+        //
+        // GET: /Us/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Us/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(New1 new1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.users.Add(new1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(new1);
+        }
+
+        //
+        // GET: /Us/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            New1 new1 = db.users.Find(id);
+            if (new1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(new1);
+        }
+
+        //
+        // POST: /Us/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(New1 new1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(new1).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(new1);
+        }
+
+        //
+        // GET: /Us/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            New1 new1 = db.users.Find(id);
+            if (new1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(new1);
+        }
+
+        //
+        // POST: /Us/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            New1 new1 = db.users.Find(id);
+            db.users.Remove(new1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ New/New/Models/New1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace New.Models
+{
+    public class New1
+        
+    {   public int Id{get; set;}
+        [Required(ErrorMessage="enter the name")]
+        [Display(Name="Name of User")]
+        public string name{get; set;}
+        [Required (ErrorMessage="enter the username")]
+        [Display(Name = "UserName of User")]
+        public string uname { get; set; }
+        [Required (ErrorMessage="enter the Password")]
+        [Display(Name = "Enter Password")]
+        [DataType(DataType.Password)]
+        public string password{get; set;}
+    }
+}
